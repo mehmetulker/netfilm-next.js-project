31cd6e6f2ca8aedffa5c5baf7b135cceb6005e0b
Mehmet ÜLKER <mehmet-ulker@hotmail.com>
2024-10-16 22:35:10 +0300

    footer completed.

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
+import style from "@/components/footer/styles.module.css"
 
 function index() {
   return (
-    <div>index</div>
+    <footer className={style.footer}>
+      Made with ❤️ by&nbsp;
+      
+    <Link href="https://github.com/mehmetulker/netfilm-next.js-project">
+    Mehmet Ülker</Link></footer>
   )
 }
 
