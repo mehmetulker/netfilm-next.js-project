3f4317796b2de3c1c410abd1eac06bd422e35419
Mehmet ÜLKER <mehmet-ulker@hotmail.com>
2024-10-25 22:06:49 +0300

    Moives-section created. HomeConteiner page has been added.

--- containers/home/index.js
+++ containers/home/index.js
@@ -4,11 +4,14 @@ import Genres from   "@/mocks/genres.json"
 import FeaturedMovie from '@/components/featured-movie'
 
 import Categories from '@/components/catagories'
+import MoviesSection from '@/components/movies-seciton'
 
 function HomeContainer() {
   return (
     <div><FeaturedMovie movie={Movies.results[0]} />
-      <Categories categories={Genres.genres.slice(0,5)} />
+      <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection title="Popular Films" movies={Movies.results.slice(1,7)} />
+      <MoviesSection title="Your Fovorites " movies={Movies.results.slice(7,13)}/>
     
     </div>
   )
