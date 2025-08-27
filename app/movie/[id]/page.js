725f44a98d3f82044a15754c0ab078395f254fb7
mehmet <mehmet-ulker@hotmail.com>
2025-08-27 23:44:45 +0300

    fix(movie): add notFound handling for unsuccessful movieDetail in [id]/page.js

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import { notFound } from 'next/navigation'
-import MovieContainer from '@/containers/movie'
-import Movies from "@/mocks/movies.json"
+import React from "react";
+import { notFound } from "next/navigation";
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
 
-const API_URL = "https://api.themoviedb.org/3"
+const API_URL = "https://api.themoviedb.org/3";
 
 const getMovie = async (movieId) => {
-    const response = await fetch(
-      `${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}`
-    );
-   return response.json();
+  const response = await fetch(
+    `${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+  return response.json();
 };
 
-
 async function MoviePage({ params, searchParams }) {
-
-    
   // console.log(params)
-  const movieDetail = await getMovie(params.id)
+  const movieDetail = await getMovie(params.id);
 
+  //console.log(movieDetail)
 
-  if (!movieDetail) {
+  if (movieDetail.success === false) {
     notFound();
   }
 
-   if (searchParams.error==="true") {
-  throw new Error("Error happened")
+  if (searchParams.error === "true") {
+    throw new Error("Error happened");
   }
   return (
-    <div><MovieContainer movie={movieDetail}/></div>
-  )
+    <div>
+      <MovieContainer movie={movieDetail} />
+    </div>
+  );
 }
 
-export default MoviePage
+export default MoviePage;
